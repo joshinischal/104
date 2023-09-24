77113008b7fe60f176d76e6a082f92d6b781f16e
nischal <nischaljoshi22@gmail.com>
2023-09-24 23:22:43 +0545

    add

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+const name=["apple","banana"]
+console.log(name)
+console.log(name[0])
+const person ={
+name : "nischal",
+age:"35",
+profession :"service"
+}
+ console.log(person)
+ console.log(person.name)
+ console.log(person.age)
+//console.log(name[0])
